f9e070bc9177471f6b73d510f37170313a6f0ed2
fjvaldes1 <fjvaldes1@uc.cl>
2022-03-22 16:33:29 -0300

    first commit

---
+++ src/helpers/collections.js
@@ -0,0 +1,19 @@
+export const networkCollections = {
+  
+  "0x4": [
+    {
+      image:
+        "https://lh3.googleusercontent.com/sCktmhddBePRNRfmG4cJ5Tbf-Ht4r8X3dCeBEKJ_Cau8FJ4HcS_7eY49H2prW_PndY8JSx-4CVQs5wgmlxJsLlKP=s0",
+      name: "Rinkeby Bricks",
+      addrs: "0xe0d8d7b8273de14e628d2f2a4a10f719f898450a",
+    },
+    {
+      image:
+        "https://lh3.googleusercontent.com/B54ESgO4A9fgU8QX5P8k1mKHDzTmjHf1rUV6TVBXY-zKhgPKJfzxTGg8zMoSZJr53Fje6nE0Iu5XFgBjE2HU-6QmZA=s0",
+      name: "Rinkeby Moons",
+      addrs: "0xdf82c9014f127243ce1305dfe54151647d74b27a",
+    },
+  ],
+};
+
+export const getCollectionsByChain = (chain) => networkCollections[chain];
